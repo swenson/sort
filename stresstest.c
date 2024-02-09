a8f8f4ec3c9cb2a7aec7c10b22a983ad038d4696
Christopher Swenson <swenson@swenson.io>
2024-02-08 16:42:37 -0800

    Implement lrand48/srand48

--- stresstest.c
+++ stresstest.c
@@ -62,10 +62,20 @@ static __inline int simple_cmp(const void *a, const void *b) {
 #ifdef _WIN32
 
 #include <time.h>
+static __inline void srand48(long seed) {
+  srand(seed);
+}
+
+static __inline long lrand48(void) {
+  int x;
+  rand_s(&x);
+  return x & 0x7fffffff;
+}
+
 static __inline double utime(void) {
-    struct timespec ts;
-    timespec_get(&ts, TIME_UTC);
-    return 1000000.0 * ts.tv_sec + ts.tv_nsec / 1000.0;
+  struct timespec ts;
+  timespec_get(&ts, TIME_UTC);
+  return 1000000.0 * ts.tv_sec + ts.tv_nsec / 1000.0;
 }
 #else
 
@@ -240,7 +250,7 @@ int run_tests(int64_t *sizes, int sizes_cnt, int type) {
   int64_t * dst = (int64_t *)malloc(MAXSIZE * sizeof(int64_t));
   printf("-------\nRunning tests with %s:\n-------\n", test_names[type]);
   TEST_STDLIB(qsort);
-#if !defined(__linux__) && !defined(__CYGWIN__)
+#if !defined(__linux__) && !defined(__CYGWIN__) && !defined(_WIN32)
   TEST_STDLIB(heapsort);
   TEST_STDLIB(mergesort);
 #endif
